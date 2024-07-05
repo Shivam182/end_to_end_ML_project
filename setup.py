0d6237f5756e8d96be16b9c71a446dc0e5f3ebd3
Shivam Yadav <yadavshivam24012002@gmail.com>
2024-07-05 12:24:01 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+# this file enables us to build our ML application as a package itself that will help anyone else to intall and use it.
+
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    ## this fxn will return the list of requirements 
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+name ='MlProject',
+version='0.0.1',
+author='Shivam Yadav',
+author_email='yadavshivam24012002@gmail.com',
+packages=find_packages(),
+install_requires= get_requirements('requirements.txt')
+
+
+
+)
